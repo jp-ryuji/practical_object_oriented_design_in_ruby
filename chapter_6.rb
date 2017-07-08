99a0076b3dbf9c27fd33e1ed7020692a99e1c20b
Ryuji Eguchi <jp.ryuji@gmail.com>
2017-07-08 15:57:12 +0900

    Add RoadBike and MountainBike with inheritance

--- chapter_6.rb
+++ chapter_6.rb
@@ -1,16 +1,59 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :size, :chain, :tire_size
 
-  def initialize(args)
+  def initialize(args = {})
     @size = args[:size]
-    @tape_color = args[:tape_color]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
   end
 
   def spares
     {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
+      tire_size: tire_size,
+      chain: chain,
     }
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    raise NotImplementedError, "This #{self.class} cannnot respond to:"
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def initialize(args)
+    @tape_color = args[:tape_color]
+    super(args)
+  end
+
+  def spares
+    super.merge(tape_color: tape_color)
+  end
+
+  def default_tire_size
+    '23'
+  end
+end
+
+class MountainBile < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+
+  def default_tire_size
+    '2.1'
   end
 end
