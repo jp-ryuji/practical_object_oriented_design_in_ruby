dce095413083406158f0ac3d03abd1527d06017c
Ryuji Eguchi <jp.ryuji@gmail.com>
2017-07-09 13:37:10 +0900

    Refactor with factory

--- chapter_8.rb
+++ chapter_8.rb
@@ -24,7 +24,7 @@ class Parts
   end
 
   def spares
-    parts.select { |part| part.needs_spare }
+    select { |part| part.needs_spare }
   end
 end
 
@@ -38,46 +38,45 @@ class Part
   end
 end
 
-class RoadBikeParts < Parts
-  attr_reader :tape_color
-
-  def post_initialize(args)
-    @tape_color = args[:tape_color]
-  end
-
-  def local_spares
-    { tape_color: tape_color }
-  end
-
-  def default_tire_size
-    '23'
-  end
-end
-
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
-
-  def post_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
-
-  def local_spares
-    { rear_shock: rear_shock }
-  end
-
-  def default_tire_size
-    '2.1'
+# This factory knows the structure config.
+# Then config can be kept simple.
+module PartsFactory
+  def self.build(config, part_class = Part, parts_class = Parts)
+    parts_class.new(
+      config.collect do |part_config|
+        part_class.new(
+          name: part_config[0],
+          description: part_config[1],
+          needs_spare: part_config.fetch(2,  true)
+        )
+      end
+    )
   end
 end
+
+road_config = [
+  ['chain', '10-speed'],
+  ['tire_size', '23'],
+  ['tape_color', 'red']
+]
+
+moutain_config = [
+  ['chain', '10-speed'],
+  ['tire_size', '2.1'],
+  ['front_shock', 'Manitou', 'red'],
+  ['rear_shock', 'Fox']
+]
+
+road_parts = PartsFactory.build(road_config)
+mountain_parts = PartsFactory.build(mountain_config)
 
 # road_bike = Bicycle.new(size: 'L', parts: RoadBikeParts.new(tape_color: 'read'))
 # road_bike.size
 # road_bike.spares
 #
 
-chain = Part.new(name: 'chain', description: '10-speed')
-road_tire = Part.new(name: 'chain', description: '10-speed')
-front_shock = Part.new(name: 'front_shock', description: 'Manitou', needs_spare: false)
-road_bike_parts = Parts.new([chain, road_tire, tape])
+# chain = Part.new(name: 'chain', description: '10-speed')
+# road_tire = Part.new(name: 'chain', description: '10-speed')
+# front_shock = Part.new(name: 'front_shock', description: 'Manitou', needs_spare: false)
+# road_bike_parts = Parts.new([chain, road_tire, tape])
 road_bike = Bicycle.new(size: 'L', parts: road_bike_parts)
